5fc0fd21f79539f77eed35add44324caa7e3c2c0
Ben Wu <ben@jxcb.xyz>
2023-09-10 00:26:17 +0700

    feat/create new url endpoints

--- api/app.py
+++ api/app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+
+@app.route("/lost_items")
+def listing():
+    return render_template("items-list.html")
 
 
 if __name__ == "__main__":
