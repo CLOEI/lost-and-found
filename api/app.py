c3c0eb76d07177548bc70329e1b27d8066a52494
Ben Wu <ben@jxcb.xyz>
2023-09-09 23:51:20 +0700

    feat/animations (broken)

--- api/app.py
+++ api/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
